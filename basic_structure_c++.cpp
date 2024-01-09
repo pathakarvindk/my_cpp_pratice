6891ce907136277745a07319de570091d9598146
Arvind <pathakarvindk@gmail.com>
2024-01-09 10:10:22 +0530

    Understaing the basic structure of a c++ code

---
+++ basic_structure_c++.cpp
@@ -0,0 +1,11 @@
+// Online C++ compiler to run C++ program online
+#include <iostream> // iostream is a header file for C++ programs that is used for input and output
+using namespace std; // This is used to identify the location of cout function inside iostream
+
+// ALways use data type for c++ functions
+int main() {
+    // Write C++ code here
+    cout << "Hello world!";
+
+    return 0; // Return 0 is signal for successful termination of program
+}
