b45138d7b27890eba37c23c478b02a57ac41fef3
Arvind <pathakarvindk@gmail.com>
2024-01-09 10:23:01 +0530

    Variables and comments explnation

---
+++ variables_comments.cpp
@@ -0,0 +1,28 @@
+// Low level language direct conversion between machine and operator
+//  High level language layer of abstarction above/over low level
+
+/* Variables in cpp
+   Variables are conatainers to store data
+   int: Integer
+   float: Float
+   char: Character
+   double: Double
+
+*/
+
+/* USED FOR MULTIPLE LINE COMMENTS*/
+
+# include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int a = 1;
+    int b = 2;
+
+    int sum = a + b;
+    cout<<"Sum is: "<<sum<<endl;
+
+    return sum;
+}
