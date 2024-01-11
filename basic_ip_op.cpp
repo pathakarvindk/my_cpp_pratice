6fda058e965afb492dd6d6a8742e85a89695172f
Arvind <pathakarvindk@gmail.com>
2024-01-11 10:32:30 +0530

    Basic i/p and o/p in cpp

---
+++ basic_ip_op.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num1, num2;
+    cout<<"Enter the velue of num1: \n"; //Insertion operator
+
+    cin>>num1; //Extraction operator
+
+    cout<<"Enter the velue of num2: \n";
+    cin>>num2;
+
+    cout<<"The sum of num1 and num2 is: \n"<<num1+num2<<endl;
+
+    return 0;
+}
