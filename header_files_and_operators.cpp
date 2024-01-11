fe7f622f9c909eb1e3dc64d64a86cd3c86ccdcf5
Arvind <pathakarvindk@gmail.com>
2024-01-11 11:41:22 +0530

    Header files and operators in C++

---
+++ header_files_and_operators.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+// There are two types of header files
+// 1. System header files: Comes with compiler
+// 2. User defined header files: Written by programmer
+
+using namespace std;
+
+int main(){
+    int a = 5, b = 2;
+    cout <<"Hello, world!" << endl;
+
+    // Operators in C++
+    // 1. Arithmatic operator
+    // +, -, X, /, %, ++, --, ++a, --a
+
+// 2. Assignment operators: Used to assigned values to variables
+    int ab = 3; 
+
+// 3. Comparision operators
+    // ==, <=, >=, !=, >, <
+
+// 4. Logical operators
+    // == && <, Basically more than one conditions
+    // && --> And
+    // || --> or
+    // ! --> Not
+
+// 5. 
+
+    return 0;
+}
